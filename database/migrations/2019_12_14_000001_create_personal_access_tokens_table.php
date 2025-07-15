af013a2cee3ca49cc7a924c4d03ef6c9f16a7f4a
shichengjian <3038460174@qq.com>
2025-07-15 12:58:04 +0800

    发布配置文件、迁移文件

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -13,13 +13,17 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('personal_access_tokens');
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
+            // $table->morphs('tokenable');
+            $table->string('tokenable_type', 191); // 控制字段长度
+            $table->unsignedBigInteger('tokenable_id');
+            $table->index(['tokenable_type', 'tokenable_id']); // 创建复合索引
+            $table->string('name',125);
+            $table->string('token', 125)->unique();
+            $table->text('abilities');
+            $table->timestamp('last_used_at');
             $table->timestamps();
         });
     }
